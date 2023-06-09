c90f01f6602c8d6a61f22e544bbb51b8ee43d060
amiribnjaman <amiribnjaman@gmail.com>
2023-06-09 17:27:33 +0600

    Navbar almost done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import 'flowbite';
 import Navbar from './Components/Shared/Navbar';
+import Home from './Components/Home/Home';
+
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
