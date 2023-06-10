d813ce2007a3e5fdec353d0b23b69f3e898a7397
amiribnjaman <amiribnjaman@gmail.com>
2023-06-10 11:53:52 +0600

    404 Not Found content updated

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import NotFoundImg from '../../Assets/Images/404.gif'
 
 export default function NotFound() {
     return (
 
-        <div className='w-[90%] mx-auto my-12'>
+        <div className='w-[90%] mx-auto my-20'>
             <div className='w-full md:ml-4'>
                 {/* Heading */}
-                <div>
-                    <h1 className='text-center text-[32px] text-red-500 font-bold mb-1 leading-tight'>404! Not Found!</h1>
+                <div className='w-[240px] h-[120px] mx-auto'>
+                 <img className='w-full' src={NotFoundImg} alt="" />
                 </div>
             </div>
         </div>
