35f204c9c26c6eaf06b4d0fcbb49d4608fe6a3d9
amiribnjaman <amiribnjaman@gmail.com>
2023-06-10 19:22:57 +0600

    Contact and Faq pages updated

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -6,15 +6,38 @@ export default function Faq() {
         <div className='w-[90%] mx-auto my-12'>
             <div className='md:w-[65%] md:ml-4'>
                 {/* Heading */}
-                <div>
+                <div className='mb-4'>
                     <h1 className=' text-[52px] font-bold mb-1 leading-tight'>FAQ's</h1>
-                    <h3 className='text-3xl '>How can i subsribe Desh Career Email?</h3>
-                    <p className='mb-4'>
-                    You can subscribe this newsletter from . You input you email, then follow instruction. After email confirmation, your email address is added in our database. </p>
-
-                    <h3 className='text-3xl'>I am a subscriber of Desh Career, but I don't find your email. Why?</h3>
-                    <p className='mb-4'>Please check your email. May be our email goes to Promotion tab. If you find our email in this tab, please move it to primary tab. Than you will find this email in primary tab. </p>
                 </div>
+
+                {/* Accordions section */}
+
+                <div id="accordion-color" data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
+                    <h2 id="accordion-color-heading-1">
+                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
+                            <span>How can i subsribe Desh Career Email?</span>
+                            <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-color-body-1" class="hidden" aria-labelledby="accordion-color-heading-1">
+                        <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">You can subscribe this newsletter from . You input you email, then follow instruction. After email confirmation, your email address is added in our database</p>
+                        </div>
+                    </div>
+                    <h2 id="accordion-color-heading-2">
+                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-2" aria-expanded="false" aria-controls="accordion-color-body-2">
+                            <span>I am a subscriber of Desh Career, but I don't find your email. Why?</span>
+                            <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </h2>
+                    <div id="accordion-color-body-2" class="hidden" aria-labelledby="accordion-color-heading-2">
+                        <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
+                            <p class="mb-2 text-gray-500 dark:text-gray-400">Please check your email. May be our email goes to Promotion tab. If you find our email in this tab, please move it to primary tab. Than you will find this email in primary tab.</p>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </button>
-                    <div id="mega-menu-full-cta" className={showMenu === true ? 'items-center justify-between  md:flex md:w-auto md:order-1 w-full' : 'hidden'}>
+                    <div id="mega-menu-full-cta" className={showMenu === true ? 'items-center justify-between md:flex md:w-auto md:order-1 w-full' : 'hidden'}>
                         <ul class="flex flex-col md:mt-4 lg:mt-0 mt-4 font-medium md:flex-row md:space-x-2 lg:space-x-4">
                             <li className='px-1 lg:px-1 py-2 md:px-0.5'>
                                 <CustomLink to="/" class="flex font-bold py-2 md:pl-1 pl-3 text-md pr-1 text-black border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home
